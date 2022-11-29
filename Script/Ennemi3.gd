527bd49155c0dd78da10ea93ffb5a323fc522497
2133752 <2233752@carrefour.cegepvicto.ca>
2022-11-29 13:30:12 -0500

    Ennemie 3

--- Script/Ennemi3.gd
+++ Script/Ennemi3.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 
-const GRAVITY = 300
+const GRAVITY = 140
 
 onready var _animated_sprite = $Apparence
 var velocity = Vector2()
@@ -11,4 +11,7 @@ func _physics_process(delta):
 	_animated_sprite.play("Mort");
 	_animated_sprite.scale.x = -1
 	
-	velocity = move_and_collide(velocity)
+	if is_on_floor():
+		_animated_sprite.visible = false
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
