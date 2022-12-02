19a4e5edcaf66eeb2418cc8a8b4f671580571a18
2133752 <2233752@carrefour.cegepvicto.ca>
2022-12-02 16:51:43 -0500

    Ajout des modif de constant

--- Script/Joueur.gd
+++ Script/Joueur.gd
@@ -6,7 +6,7 @@ onready var _animated_sprite = get_node("Apparence")
 const GRAVITY = 200
 const WALK_SPEED = 100
 const JUMP_FORCE = 400
-
+var index = 0
 var velocity = Vector2()
 
 
@@ -26,9 +26,9 @@ func _physics_process(delta):
 		_animated_sprite.play("Innactif")
 		velocity.x = lerp(velocity.x, 0, 0.1)
 		
-	if Input.is_action_pressed("ui_up") and is_on_wall():
+	if Input.is_action_pressed("ui_up") and is_on_floor():
 		velocity.y = -JUMP_FORCE
 		_animated_sprite.play("SauterArme")
 		
 		
-	velocity = move_and_slide(velocity, Vector2())
+	velocity = move_and_slide(velocity, Vector2.UP)
